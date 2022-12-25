e1bb3ca3c28a2145bc42de8b745c53b7cddcaf28
Mustafa Fayazi <mustafafiazy01@gmail.com>
2022-12-25 10:18:31 -0800

    Can you see my name?

--- src/collobrations/Alish.java
+++ src/collobrations/Alish.java
@@ -3,5 +3,7 @@ package collobrations;
 public class Alish {
     public static void main(String[] args) {
         System.out.println("Test2");
+        System.out.println("Can you see my name?");
     }
+
 }
