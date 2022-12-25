b36f69a982c2a0f546dc76b23f36e97006d7302a
sehriban <cayir.sehriban@gmail.com>
2022-12-25 13:18:49 -0500

    Test3

--- src/collobrations/Alish.java
+++ src/collobrations/Alish.java
@@ -3,5 +3,10 @@ package collobrations;
 public class Alish {
     public static void main(String[] args) {
         System.out.println("Test2");
+
+
+        System.out.println("Sherry");
     }
+
+
 }
