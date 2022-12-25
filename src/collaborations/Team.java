b4b4f79bee9ac255015e8f535b64291af07fed73
alishertussupbayev <alish.tusupbaev@gmail.com>
2022-12-25 12:43:06 -1000

    create single inheritance

---
+++ src/collaborations/Team.java
@@ -0,0 +1,57 @@
+package collaborations;
+
+public class Team {
+    private  String name;
+    private String power,weakness;
+
+    public Team(String name, String power, String weakness) {
+        this.name = name;
+       setPower(power);
+        setWeakness(weakness);
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPower() {
+        return power;
+    }
+
+    public void setPower(String power) {
+        if(power.equalsIgnoreCase("finish")){
+            System.err.println("Impossible move forward.All Team hurt");
+            System.exit(1);
+        }
+        this.power = power;
+    }
+
+    public String getWeakness() {
+        return weakness;
+    }
+
+    public void setWeakness(String weakness) {
+        if(weakness.equalsIgnoreCase("Lazy")||weakness.equalsIgnoreCase("no time")||weakness.equalsIgnoreCase("impossible")){
+            System.err.println("Impossible move forward.All Team hurt");
+            System.exit(1);
+
+        }
+        this.weakness = weakness;
+    }
+
+    public  void  Retro(){
+
+    }
+
+    @Override
+    public String toString() {
+        return "Team{" +
+                "name='" + name + '\'' +
+                ", power='" + power + '\'' +
+                ", weakness='" + weakness + '\'' +
+                '}';
+    }
+}
---
+++ src/collaborations/TeamMember.java
@@ -0,0 +1,23 @@
+package collaborations;
+
+import Alisher.Team;
+
+import java.time.LocalDate;
+
+public class TeamMember extends Team {
+    public LocalDate yesterday;
+    public LocalDate today=LocalDate.now();
+    public LocalDate tomorrow;
+    public TeamMember(String name, String power, String weakness) {
+        super(name, power, weakness);
+    }
+
+    @Override
+    public void Retro() {
+        System.out.println(getName()+": did { spent some time with son,finished couple warm up tasks on CodingBat }"+(yesterday=today.minusDays(1)));
+        System.out.println(getName()+": working on { Joined on group meeting(git,tools), now watching git second video,finish short videos on git.Night will start new weekly videos }"+(today));
+        System.out.println(getName()+": will do {Join on group meeting (STLC), finish one of the weekly videos  }"+(tomorrow=today.plusDays(1)));
+    }
+
+
+    }
