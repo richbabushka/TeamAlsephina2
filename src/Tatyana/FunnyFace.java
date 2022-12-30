ac58cab1c6a75908cf6782b01f30c8ad6b14421d
richbabushka <latatsvt@gmail.com>
2022-12-30 20:41:44 +0800

    FunnyFace file added

---
+++ src/Tatyana/FunnyFace.java
@@ -0,0 +1,15 @@
+package Tatyana;
+
+public class FunnyFace {
+
+    public static void main(String[] args)
+    {
+        System.out.println(" +\"\"\"\"\"+ ");
+        System.out.println("[| o o |]");
+        System.out.println(" |  ^  | ");
+        System.out.println(" | '-' | ");
+        System.out.println(" +-----+ ");
+    }
+}
+
+
