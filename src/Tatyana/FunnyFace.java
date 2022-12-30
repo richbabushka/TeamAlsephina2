efc44869e9b0a81dc896dfb3ce66982458b86be9
richbabushka <latatsvt@gmail.com>
2022-12-30 20:45:09 +0800

    "Happy New Year" commit added

--- src/Tatyana/FunnyFace.java
+++ src/Tatyana/FunnyFace.java
@@ -4,6 +4,8 @@ public class FunnyFace {
 
     public static void main(String[] args)
     {
+        // Hi, Team ! I am in GitHub with you! Happy New Year!
+
         System.out.println(" +\"\"\"\"\"+ ");
         System.out.println("[| o o |]");
         System.out.println(" |  ^  | ");
